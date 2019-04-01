56457f00760c2ede33274b17b4800b57f7852a3d
inker <anton.veselyev@gmail.com>
2019-04-01 15:51:11 +0100

    v0.1.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-BUMP_TYPE=$1
-
-echo "bump type: $BUMP_TYPE"
+BUMP_TYPE=${1:-patch}
 
 npm update
 npm run build
 
-VERSION=$(npm --no-git-tag-version version patch)
+VERSION=$(npm --no-git-tag-version version ${BUMP_TYPE})
 echo "new version: $VERSION"
 
 git add .
