fa89541287e31f9188ba7ade9a155d78140cdbf9
inker <anton.veselyev@gmail.com>
2020-04-23 16:00:49 +0100

    fix

--- src/imagesToVideo/getFfmpegOptions.ts
+++ src/imagesToVideo/getFfmpegOptions.ts
@@ -2,8 +2,10 @@ type Format = 'wmv' | 'mp4'
 
 const options = {
   mp4: {
-    f: 'mp4',
+    f: 'mpegts',
     vcodec: 'libx264',
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    pix_fmt: 'yuv420p',
     tune: 'zerolatency',
     preset: 'ultrafast',
     movflags: 'faststart',
